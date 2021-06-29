a40b64a2b711225850144926f7288520208366d0
lemjiamo <luismejia69@gmail.com>
2021-06-29 16:08:41 +0000

    fix sintaxerror

--- console.py
+++ console.py
@@ -157,8 +157,7 @@ class HBNBCommand(cmd.Cmd):
             instance_dict = storage.all()
             for key in list:
                 instance_dict = storage.objects.get(key)
-                instance_object = eval(instance_dict.get('__class__'))
-                (**instance_dict)
+                instance_object = eval(instance_dict.get('__class__'))                (**instance_dict)
                 instances_list.append(instance_object.__str__())
             print(instances_list)
 
