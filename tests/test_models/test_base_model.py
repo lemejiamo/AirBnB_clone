89bd0b6f7c5f38d42e95f8671a3c03ec343f2bf4
lemejiamo <luismejia69@gmail.com>
2021-06-30 18:23:30 -0500

    test update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,6 +8,8 @@
 from models.base_model import BaseModel
 import unittest
 import os
+from datetime import datetime
+import re
 
 
 class Test_airbnb(unittest.TestCase):
@@ -20,86 +22,46 @@ class Test_airbnb(unittest.TestCase):
             test to CONSTRUCTOR (__init__)
             in class base_model
         """
-
         model_dict = {'name': 'holberton', 'edad': '23'}
+
         test_model = BaseModel(**model_dict)
 
         # test to verify the instance
         self.assertIsInstance(test_model, BaseModel)
 
         # test to verify the type of variable
-        base_type_name = type(test_model.name)
-        print(type(test_model.name))
-        self.assertEqual(base_type_name, str)
 
-        base_type_create = type(test_model.created_at)
-        print(base_type_create)
-        self.assertEqual(type(test_model.created_at), base_type_create)
+        self.assertEqual(type(test_model.created_at), datetime)
 
-        base_type_update = type(test_model.update_at)
-        self.assertEqual(type(test_model.update_at), base_type_update)
+        self.assertEqual(type(test_model.update_at), datetime)
 
-        baase_type_id = type(test_model.id)
         self.assertEqual(type(test_model.id), str)
 
-        # test to verify if KWARGS contains something
+        self.assertEqual(type(test_model.name), str)
 
+        # test to verify the  assigment of var
+        self.assertEqual(test_model.name, "Holberton")
+
+        self.assertEqual(test_model.edad, 23)
+
+        self.assertTrue(test_model.id)
+
+        updated_pre = test_model.updated_at
+        test_model.save()
+        self.assertTrue(test_model.updated_at > updated_pre)
+
+        # test to verify if KWARGS contains something
         test_model2 = BaseModel(**model_dict)
         self.assertEqual(test_model2.name, 'holberton')
+        self.assertEqual(test_model2.edad, 23)
 
-        with self.assertRaises(TypeError):
-            BaseModel(2)
 
-        test_list = list()
-        with self.assertRaises(TypeError):
-            BaseModel(test_list)
-
-    def test_str_method(self):
-        """
-            Test to verify the print of magic __str__ method
-        """
-        model_dict = {'name': 'holberton', 'edad': '23', 'id': '0'}
-        test_model = BaseModel(**model_dict)
-
-        print(test_model)
-
-        pass
-
-    def test_save(self):
-
-        # Test to check update function
-        model_dict = {'name': 'holberton', 'edad': '23', 'id': '0'}
-        test_model = BaseModel(**model_dict)
-        test_model.save()
-        self.assertNotEqual(test_model.created_at, test_model.update_at)
-
-    def test_to_dict_method(self):
-        """
-            Test to verify to_dict_method
-        """
-        # comprobar si la salida es diferente
-        # comprobar el formato de salida de la fecha debe ser STR
-
-        pass
-
-    def test_to_kwargs(self):
-        # verificar que le valor de kwargs sea cero cuando no se pase nada
-        pass
-
-    def test_to_storage(self):
-        # verify
-        pass
-
-    def test_save_storage(self):
-        # verificar que las listas no sean nulas o vacias
-        pass
-
-    def test_reload(self):
-        # verificar si el archivo existe
-        pass
-
-# |------------ENTRY POINT------------|
-
+    def test_str(self):
+        '''[check __str__ method]'''
+        my_model = BaseModel()
+        r = re.compile("\[BaseModel\] (.*) {.*}")
+        my_str = my_model.__str__()
+        self.assertIsNotNone(r.match(my_str))
 
 if __name__ == "__main__":
     unittest.main()
